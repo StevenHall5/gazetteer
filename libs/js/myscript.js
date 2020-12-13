037d80207c4745675f4bc76e885b680d598be3b6
StevenHall5 <73425435+StevenHall5@users.noreply.github.com>
2020-12-13 15:55:00 +0000

    Icon added, bootstrap error fixed, locator turned off, borders added, geo.JSON added

--- libs/js/myscript.js
+++ libs/js/myscript.js
@@ -1,29 +1,39 @@
 // navigator.geolocation for current user location
-var x = document.getElementById("demo");
+// var x = document.getElementById("demo");
 
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
+// function getLocation() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(showPosition);
+//   } else { 
+//     x.innerHTML = "Geolocation is not supported by this browser.";
+//   }
+// }
 
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
-}
+// function showPosition(position) {
+//   x.innerHTML = "Latitude: " + position.coords.latitude + 
+//   "<br>Longitude: " + position.coords.longitude;
+// }
 
 // initialise map
 
 var map = L.map( 'mapid');
-map.locate({setView: true, maxZoom: 16});
+map.locate({setView: true, maxZoom: 5});
 
 L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo( map );
 
+function style(feature) {
+    return {
+        weight: 2,
+        opacity: 1,
+        color: 'black',
+        dashArray: '3',
+        fillOpacity: 0
+    };
+}
 
+L.geoJson(countryData, {style: style}).addTo(map);
 
 
 
